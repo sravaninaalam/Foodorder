35c0e02e90e4b5eaa0f2b51c38bbe5755b3d9261
sravaninaalam <129977519+sravaninaalam@users.noreply.github.com>
2024-01-22 16:03:33 +0530

    aboutpage✅

--- src/components/About.js
+++ src/components/About.js
@@ -1,40 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import User from './User'
 import Userclass from './Userclass'
 
 
 
-export const Section=({title,Body})=>{
+export const Section=({title,Body,visible,setShow,hide})=>{
   return(
-   <div>
-       <h2>{title}</h2>
-       <h3>{Body}</h3>
+   <div className='my-4 md:p-2 md:mx-10 mx-1 border border-blue-200 bg-lime-50'>
+       <h2 className='font-bold'>{title}</h2>
+       {visible
+            ?<div>
+                <button onClick={()=>hide()}>HIDE</button>
+                <h3>{Body}</h3>
+            </div>
+       :<button onClick={()=>setShow()}>SHOW</button>
+       }
+     
    </div>
   )
 }
 const About = () => {
+
+     const[show,setShow]=useState()
+
   return (
    <>
-      <Section title={"Developed By"} 
+   <div  className='mt-5 md:mx-10 mx-0'>
+      <Section title={"Developed By"} visible={show=='developer'} setShow={()=>setShow('developer')} hide={()=>setShow(null)}
       Body={ <Userclass name={"Naalam Sravani"} location={"Vizianagaram,Andhra Pradesh,India"} mail={"nalamsravani2016@gmail.com"}/>}
       />
-      <Section title={"Techstack"} 
-     Body={
-        <ul>
-          <li>React Slick</li>
-          <li>Tailwind</li>
-        </ul>
+      <Section title={"Techstack"}  visible={show=='techstack'} setShow={()=>setShow('techstack')}
+      hide={()=>setShow(null)} Body={
+        <ul className='mx-8 my-3 list-disc'>
+        <li><span className='font-bold'>React: </span> The heart and soul of our app, ensuring dynamic and responsive user interfaces.</li>
+        <li><span className='font-bold'>Parcel: </span> A blazing-fast web application bundler, keeping our app performant.</li>
+        <li><span className='font-bold'>Tailwind CSS :</span> Making our UI sleek, stylish, and customizable</li>
+        <li><span className='font-bold'>Redux Toolkit: </span> For state management that's both efficient and developer-friendly.</li>
+        <li>For effortless navigation <span className='font-bold'>React Router</span> is used which enables Client side routing</li>
+      </ul>
       }/>
-      <Section title={"Feautures"}Body={
-        <ul>
-          <li>UI design</li>
-          <li>Navigation</li>
-        </ul>
+      <Section title={"Feautures"} visible={show=='feautures'} setShow={()=>setShow('feautures')}
+       hide={()=>setShow(null)} Body={
+        <ul  className='mx-8 my-3 list-disc'>
+                 <li> Explore Delicious Menus offered by Top Restaurants</li>
+                 <li>Swiggy's Live Api data</li>
+                 <li> Effortless Ordering</li>
+                 <li>Dazzling Shimmer UI</li>
+                 <li>Search Restaurants</li>
+                 <li> Error Handling - 404 Page</li>
+                 <li> Fully Responsive</li>
+              </ul>
       }/>
-        {/* <div className='mx-auto text-center '>
-            <h1>Developed By</h1>
-            <Userclass name={"Naalam Sravani"} location={"Vizianagaram,Andhra Pradesh,India"} mail={"nalamsravani2016@gmail.com"}/>
-        </div> */}
+        </div>
    </>
   )
 }
--- src/components/Userclass.js
+++ src/components/Userclass.js
@@ -8,13 +8,12 @@ class Userclass extends React.Component{
         const {name,location,mail}=this.props
        
         return(
-            <div className="my-5 bg-blue-200">
+            <div className="mt-3">
             <h2 className='text-black font-medium mx-1 p-1'>Name:<span className='text-gray-600 ml-1'>{name}</span></h2>
             <h3 className='text-black font-medium mx-1 p-1'>Location: <span className='text-gray-600 ml-1'>{location}</span></h3>
             <h3 className='text-black font-medium mx-1 p-1'>Mail: <span className='text-gray-600 ml-1'>{mail}</span></h3>
         </div>
         )
     }
-
 }
 export default Userclass

ed76b500b17c8ab3d9582860af1c50841b87eded
sravaninaalam <129977519+sravaninaalam@users.noreply.github.com>
2024-01-28 21:12:52 +0530

    icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { APPLOGO } from '../utils/constants'
 import { Link } from 'react-router-dom'
 import { useSelector,useDispatch } from 'react-redux'
 import { enterLocation } from '../redux/locationSlice'
+import { Home ,BadgeInfo,ShoppingCart} from 'lucide-react'
 const Header = () => {
     const[loc,setLoc]=useState('')
     const dispatch=useDispatch()
@@ -25,9 +26,9 @@ const Header = () => {
                  value={loc} onChange={(e)=>setLoc(e.target.value)}/>
             </form>
                 <ul className='flex p-3 m-3 items-center'>
-                    <Link to='/'><li className='mx-3'>Home</li></Link>
-                   <Link to='/about'><li className='mx-3'>About</li></Link>
-                    <Link to='/cart'><li className='mx-3'>Cart -{cartItems.length}</li></Link>
+                    <Link to='/'><li className='mx-3'><Home/></li></Link>
+                   <Link to='/about'><li className='mx-3'><BadgeInfo/></li></Link>
+                    <Link to='/cart'><li className='mx-3 flex'><ShoppingCart/> -{cartItems.length}</li></Link>
                    <Link to='/login'><li className='mx-3'>Login</li></Link>
                 </ul>
             </div>

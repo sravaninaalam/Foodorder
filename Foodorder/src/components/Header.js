4e087e85da281588ada962a0a8fc5dfcd935c769
sravaninaalam <129977519+sravaninaalam@users.noreply.github.com>
2024-02-03 21:50:09 +0530

    CSS ADDED

--- Foodorder/src/components/Header.js
+++ Foodorder/src/components/Header.js
@@ -3,7 +3,7 @@ import { APPLOGO } from '../utils/constants'
 import { Link } from 'react-router-dom'
 import { useSelector,useDispatch } from 'react-redux'
 import { enterLocation } from '../redux/locationSlice'
-import { Home ,BadgeInfo,ShoppingCart} from 'lucide-react'
+import { Home ,BadgeInfo,ShoppingCart,UserSquare2} from 'lucide-react'
 import userContext from '../utils/userContext'
 const Header = () => {
     const[loc,setLoc]=useState('')
@@ -20,20 +20,21 @@ const Header = () => {
    
   return (
    <>
-        <div className='flex justify-between items-center bg-slate-50 shadow-md sticky top-0'>
-            <img src={APPLOGO} alt='applogo' className='w-16 p-2 ml-24'/>
+        <div className='flex justify-between bg-cyan-100 items-center shadow-md sticky top-0'>
+            <img src={APPLOGO} alt='applogo' className='w-16 p-2 ml-24 rounded-md'/>
             
             <div className='flex'>
             <form  className='' onSubmit={fetchingLocation}>
-                 <input type='text' placeholder='Enter Location' className='border border-black p-1 mt-6 w-48'
+                 <input type='text' placeholder='Enter Location' className='rounded-md border border-black p-1 mt-6 w-48'
                  value={loc} onChange={(e)=>setLoc(e.target.value)}/>
             </form>
                 <ul className='flex p-3 m-3 items-center'>
                     <Link to='/'><li className='mx-3'><Home/></li></Link>
                    <Link to='/about'><li className='mx-3'><BadgeInfo/></li></Link>
-                    <Link to='/cart'><li className='mx-3 flex'><ShoppingCart/> -{cartItems.length}</li></Link>
+                    <Link to='/cart'><li className='mx-3 flex'><ShoppingCart/> -
+                   <span className='bg-white rounded-full px-1 font-semibold text-red-800'>{cartItems.length}</span></li></Link>
                     {loggedUser ?<span className="font-bold mx-4 my-4 md:my-0">{loggedUser}</span>:
-                   <Link to='/login'><li className='mx-3'>Login</li></Link>}
+                   <Link to='/login'><li className='mx-3 '><UserSquare2/></li></Link>}
                 </ul>
             </div>
         </div>
